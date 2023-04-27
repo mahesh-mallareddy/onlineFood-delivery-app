0f2743fb603ec05e9fe28c53786c71d0bd1e82b0
mahesh-mallareddy <mallareddymahesh0104@gmail.com>
2023-04-27 16:33:05 +0530

    custom hook modified

--- src/components/restomenudata.js
+++ src/components/restomenudata.js
@@ -2,53 +2,43 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Restomenudata, Restohead } from "./Menudestructure";
 import { Shimmermenuui } from "./Shimmer";
-
+import { useRestomenu, useRestoheader } from "../utils/useRestomenu"
 
 const Restomenuinfo = () => {
     const { paramsid } = useParams()
 
-    const [restomenulist, setrestomenulist] = useState([])
-    const [restomenuheader, setrestomenuheader] = useState([])
+    // const [restomenulist, setrestomenulist] = useState([])
+    const restomenulist = useRestomenu(paramsid);
+    const restomenuheader = useRestoheader(paramsid);
 
-    async function getrestoinfo() {
-        const fetchurl = await fetch("http://localhost:2000/get_all_products/brand_id/" + paramsid + "/store_id/10370/source_id/13")
-        const data = await fetchurl.json()
-        const restodata = data?.data?.collections
-        const restoheaderdata = restodata[3].products[0]
-        console.log(restoheaderdata)
-        setrestomenulist(restodata)
-        setrestomenuheader(restoheaderdata)
-    };
-    useEffect(() => {
-        getrestoinfo()
-    }, [])
 
-    if (!restomenulist) return null;
 
-    return (restomenulist?.length === 0) ? (<Shimmermenuui />) : (
+    return (!restomenulist) ? (<Shimmermenuui />) : (
         <>
-            <Restohead {...restomenuheader} />
+            <Restohead {...restomenuheader} key={paramsid} />
             <div>
                 <div className="restomenudata">
-                    {restomenulist.map((collection_data, index) => {
-                        return (
-                            <>
-                                <div className="menudata">
-                                    <div className="menu_head">
-                                        <h5 key={collection_data.collection_id}>{collection_data.collection_name}</h5>
-                                    </div>
-                                    <div className="restoall">
+                    {
+                        restomenulist.map((collection_data, index) => {
+                            return (
+                                <>
+                                    <div className="menudata" key={collection_data.collection_id}>
+                                        <div className="menu_head">
+                                            <h5 > {collection_data.collection_name} </h5>
+                                        </div>
+                                        <div className="restoall">
 
-                                        {collection_data.products.map((products_data, index) => {
-                                            return (
-                                                <Restomenudata {...products_data} key={products_data.products_id} />
-                                            )
-                                        })}
+                                            {collection_data.products.map((products_data, index) => {
+                                                return (
+                                                    <Restomenudata {...products_data} key={products_data.products_id} />
+                                                )
+                                            })}
+                                        </div>
                                     </div>
-                                </div>
-                            </>
-                        )
-                    })}
+                                </>
+                            )
+                        })
+                    };
                 </div>
             </div >
         </>
---
+++ src/utils/useRestomenu.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react"
+
+export const useRestomenu = (paramsid) => {
+
+    const [restomenulist, setrestomenulist] = useState(null);
+    // const [restomenuheader, setrestomenuheader] = useState(null);
+
+    useEffect(() => {
+        getrestoinfo()
+    }, []);
+
+    async function getrestoinfo() {
+        const fetchurl = await fetch("https://foodapp-7fsj.onrender.com/get_all_products/brand_id/" + paramsid + "/store_id/10370/source_id/13");
+        const data = await fetchurl.json()
+        const restodata = data?.data?.collections
+        const restoheaderdata = restodata[3].products[0]
+        setrestomenulist(restodata)
+        // setrestomenuheader(restoheaderdata)
+    };
+
+    return restomenulist;
+};
+
+export const useRestoheader = (paramsid) => {
+    const [restomenuheader, setrestomenuheader] = useState(null);
+
+    useEffect(() => {
+        getrestoinfo()
+    }, []);
+
+    async function getrestoinfo() {
+        const fetchurl = await fetch("https://foodapp-7fsj.onrender.com/get_all_products/brand_id/" + paramsid + "/store_id/10370/source_id/13");
+        const data = await fetchurl.json()
+        const restodata = data?.data?.collections
+        const restoheaderdata = restodata[3].products[0]
+        console.log(restoheaderdata)
+
+        setrestomenuheader(restoheaderdata)
+    };
+    return restomenuheader;
+};
