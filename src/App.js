70085669c56ffc9ca366ffb26f4e556925befbbb
mahesh-mallareddy <mallareddymahesh0104@gmail.com>
2023-05-08 23:56:26 +0530

    app ui

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Nav from "./components/Navbar";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Restomenuinfo from "./components/restomenudata";
 import Restomenupage from "./components/Restomenupage";
-import Cart from "./components/cart";
+// import Cart from "./components/cart";
 import { UserAuthContextProvider } from "./utils/context/userAuthcontext";
 import Login from "./components/login";
 import Signup from "./components/signup";
 import Banner from "./components/Banner";
-
+import Error from "./components/Error";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const Body = () => {
@@ -52,15 +52,16 @@ const Approuter = createBrowserRouter([
             {
                 path: "/",
                 element: <Applayout />,
+                errorElement:<Error/>,
                 children: [
                     {
                         path: "/",
                         element: <Body />
                     },
-                    {
-                        path: "/cart",
-                        element: <Cart />,
-                    },
+                    // {
+                    //     path: "/cart",
+                    //     element: <Cart />,
+                    // },
                     {
                         path: "/support",
                         element: <Contact />,
